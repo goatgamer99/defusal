a56634fbcf3028d3b8105b2aa4b9daca2ccce6d9
goatgamer99 <6uggedsalt@proton.me>
2025-04-23 12:41:43 -0400

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,45 @@
+local repo = 'https://raw.githubusercontent.com/goatgamer99/library/refs/heads/main/'
+
+local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
+local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
+local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
+
+local Window = Library:CreateWindow({
+    Title = 'kitten.kys - Rage',
+    Center = true,
+    AutoShow = true,
+    TabPadding = 8,
+    MenuFadeTime = 0.4
+})
+
+getgenv().MainTab = Window:AddTab('Main')
+getgenv().VisualsTab = Window:AddTab('Visuals')
+getgenv().VisualsTab = Window:AddTab('Movement')
+getgenv().MiscTab = Window:AddTab('Misc')
+
+local MiscellaneousGroup = getgenv().MiscTab:AddRightGroupbox('Miscellaneous')
+
+MiscellaneousGroup:AddButton("Unload", function()
+    Library:Unload()
+end)
+
+MiscellaneousGroup:AddLabel("Menu bind"):AddKeyPicker("MenuKeybind", {
+    Default = "J",
+    NoUI = true,
+    Text = "Menu keybind"
+})
+Library.ToggleKeybind = Options.MenuKeybind
+
+Library:OnUnload(function()
+    Library.Unloaded = true
+end)
+
+ThemeManager:SetLibrary(Library)
+SaveManager:SetLibrary(Library)
+SaveManager:IgnoreThemeSettings()
+SaveManager:SetIgnoreIndexes({ 'MenuKeybind' })
+ThemeManager:SetFolder('kittenkys')
+SaveManager:SetFolder('kittenkys/defusal')
+SaveManager:BuildConfigSection(getgenv().MiscTab)
+ThemeManager:ApplyToTab(getgenv().MiscTab)
+SaveManager:LoadAutoloadConfig()
